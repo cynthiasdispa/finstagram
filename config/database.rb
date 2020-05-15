589bbf7ab34eccfcbfca16ceab8aa5acd3828dbc
Cynthia Salazar Dispa <cynthia0612@hotmail.com>
2020-05-15 01:15:29 +0000

    added configuration files

--- config/database.rb
+++ config/database.rb
@@ -1,14 +1,22 @@
 configure do
-  # Log queries to STDOUT in development
-  if Sinatra::Application.development?
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-  end
-
-  set :database, {
-    adapter: "sqlite3",
-    database: "postgres://uyotzdbesadrez:398ec94feab72996c199fdf3588711718c2e83e3b610ee1b74a221f95dbd28ce@ec2-54-147-209-121.compute-1.amazonaws.com:5432/db531k09s4jjl5"
-    # database: "db/db.sqlite3"
-  }
+    # Log queries to STDOUT in development
+    if Sinatra::Application.development?
+        set :database, {
+        adapter: "sqlite3",
+        database: "db/db.sqlite3"
+        }
+    else
+        db = URI.parse(ENV['DATABASE_URL'] || 'postgres://uyotzdbesadrez:398ec94feab72996c199fdf3588711718c2e83e3b610ee1b74a221f95dbd28ce@ec2-54-147-209-121.compute-1.amazonaws.com:5432/db531k09s4jjl5')
+        set :database, {
+        adapter: "postgresql",
+        host: db.host,
+        username: db.user,
+        password: db.password,
+        database: db.path[1..-1],
+        encoding: "utf8"
+        }
+    ​
+    end
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
